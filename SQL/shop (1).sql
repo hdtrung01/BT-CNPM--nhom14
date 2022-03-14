3ccb6aeb5f47ee9a65bd6bb2d2bad14d6d77d9b6
hdtrung01 <90234424+hdtrung01@users.noreply.github.com>
2022-03-14 18:53:26 +0700

    fix sql

--- SQL/shop (1).sql
+++ SQL/shop (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 11, 2022 lúc 11:55 AM
+-- Thời gian đã tạo: Th3 14, 2022 lúc 12:52 PM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 8.0.10
 
@@ -31,16 +31,19 @@ CREATE TABLE `sanpham` (
   `MaSP` varchar(5) NOT NULL,
   `TenSP` varchar(1000) NOT NULL,
   `LoaiSP` varchar(1000) NOT NULL,
-  `Gia` int(11) NOT NULL
+  `Gia` int(11) NOT NULL,
+  `anh` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Đang đổ dữ liệu cho bảng `sanpham`
 --
 
-INSERT INTO `sanpham` (`MaSP`, `TenSP`, `LoaiSP`, `Gia`) VALUES
-('sp001', 'Tee mặt cười', 'Top', 250000),
-('sp002', 'short basic', 'bottom', 200000);
+INSERT INTO `sanpham` (`MaSP`, `TenSP`, `LoaiSP`, `Gia`, `anh`) VALUES
+('sp001', 'Hoodie dirty coin', 'ao', 600000, '1.png'),
+('sp002', 'Hoodie dirty coin xanh', 'ao', 600000, '2.png'),
+('sp003', 'Hoodie dirty coin đỏ', 'ao', 600000, '3.png'),
+('sp004', 'cadigan dirtycoin', 'ao', 490000, '4.png');
 
 -- --------------------------------------------------------
 
@@ -61,7 +64,16 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`username`, `Password`, `Email`, `permission`) VALUES
 ('admin', '21232F297A57A5A743894A0E4A801FC3', 'Shopquanao@gmail.com', 1),
-('hdtrung', 'c2d8730c4cdd662573b0214a0183bf98', 'hdtrung01@gmail.com', 0);
+('hdtrung', 'c2d8730c4cdd662573b0214a0183bf98', 'hdtrung01@gmail.com', 0),
+('', 'hdtrung01@gmail.com', '202cb962ac59075b964b07152d234b70', 0),
+('hdtrung', '202cb962ac59075b964b07152d234b70', 'das', 0),
+('cpc', '202cb962ac59075b964b07152d234b70', 'das', 0),
+('cpc', '202cb962ac59075b964b07152d234b70', 'das', 0),
+('cpc', '202cb962ac59075b964b07152d234b70', '12', 0),
+('áds', '8277e0910d750195b448797616e091ad', 'â', 0),
+('trung', '5eac43aceba42c8757b54003a58277b5', 'trung@gmail.com', 0),
+('trung', '5eac43aceba42c8757b54003a58277b5', 'trung@gmail.com', 0),
+('trung', '202cb962ac59075b964b07152d234b70', 'trung@gmail.com', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- home_admin.php
+++ home_admin.php
@@ -1,12 +1,8 @@
-<?php include 'head.php' ?>
+<?php include 'head.php';?>
 
+<h1 class="bg-success">Trang của admin</h1>
 
-hehe
-
-
-
-
-
-
-
+<div class="text-center">
+    <a class="btn btn-success" href="update.php">Đăng sản phẩm mới </a>
+</div>
 <?php include 'foot.php' ?>
