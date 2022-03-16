be72d8ed1f197e4ad7fdf0ab7d35c1a519944eab
haiduy <86014550+hai232@users.noreply.github.com>
2022-03-16 15:49:54 +0700

    Add files via upload

--- themgiohang.php
+++ themgiohang.php
@@ -10,7 +10,14 @@
   if (!$conn){
       die("Ko ket noi duoc");
   };
-  $sql = "INSERT INTO giohang(username,MaSP,SoLuong)VALUES('$username','$id',1);";
-  mysqli_query($conn, $sql);
+  $sql = "SELECT * from giohang where username = '$username' and MaSP = '$id'";
+  $result=mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result)>0){
+    $sql = "UPDATE giohang SET soluong = soluong + 1 WHERE username='$username' AND MaSP = '$id'";
+    mysqli_query($conn, $sql);
+  }else{
+    $sql = "INSERT INTO giohang(username,MaSP,SoLuong)VALUES('$username','$id',1);";
+    mysqli_query($conn, $sql);
+    }
   header("Location:./giohang.php");
 ?>
