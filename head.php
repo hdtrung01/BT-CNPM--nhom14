82331bd8236844199e3cb241c62a139840faaee9
DucGiang <90235692+nhokcunpvp@users.noreply.github.com>
2022-03-17 14:59:56 +0700

    Update head.php

--- head.php
+++ head.php
@@ -9,6 +9,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./font/css/all.min.css">
+    <link href = "https: //fonts.googleapis.com/css2? family = Roboto: wght @ 100; 300; 400; 500; 700 & display = swap "rel =" stylesheet ">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
     <title>SHOP Quần áo!!</title>
   </head>
